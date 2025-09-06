68fb3b00fd118b52f1b0c5b3c195940e1e1aa832
freekomaiyasuaki <free.komaiyasuaki@gmail.com>
2025-09-06 16:02:51 +0900

    bingo app

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,48 +1,64 @@
 import { useEffect, useState } from "react";
 
 export default function Main() {
-  const [pokeData, setPokeData] = useState([
-    {
-      species: {
-        url: null,
-        name: null,
-        genus: null,
-        number: null,
-      },
-      image: null,
+  const [pokeData, setPokeData] = useState({
+    species: {
+      url: null,
+      name: null,
+      genus: null,
+      number: null,
     },
-  ]);
-  const [loading, setLoading] = useState(true);
+    image: null,
+  });
   const [err, setErr] = useState(null);
-  const [text, setText] = useState();
+  const [num, setNum] = useState(null);
+
+  const [bingoNum, setBingoNum] = useState([]);
+  const [stockNum, setStockNum] = useState([]);
+
+  console.log(stockNum);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/25/");
-        if (!response.ok) {
-          throw new Error("Failed!");
-        }
-        const data = await response.json();
-        console.log(data);
-        const pokeName = await fetchName(data.species.url);
-        console.log(pokeName);
-        setPokeData({
-          ...pokeData,
-          species: {
-            name: pokeName.names[0].name,
-            genus: pokeName.genera[0].genus,
-            number: pokeName.pokedex_numbers[0].entry_number,
-          },
-        });
-      } catch (err) {
-        setErr(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
+    const arr = Array.from({ length: 75 }, (_, i) => i + 1);
+    setBingoNum(arr);
   }, []);
+
+  const fetchData = async () => {
+    const bingoLen = bingoNum.length;
+    if (bingoLen === 0) return;
+    const randomNum = Math.floor(Math.random() * bingoLen);
+    const getNum = bingoNum[randomNum];
+    console.log(randomNum);
+    setNum(getNum);
+    console.log(getNum);
+    setStockNum([...stockNum, getNum].sort((a, b) => a - b));
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${getNum}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed!");
+      }
+      const data = await response.json();
+      console.log(data);
+      const pokeName = await fetchName(data.species.url);
+      console.log(pokeName);
+      setPokeData({
+        ...pokeData,
+        species: {
+          name: pokeName.names[0].name,
+          genus: pokeName.genera[0].genus,
+          number: pokeName.pokedex_numbers[0].entry_number,
+        },
+        image: data.sprites.other["official-artwork"].front_default,
+      });
+      console.log(getNum);
+      const newArr = bingoNum.filter((item) => item !== Number(getNum));
+      setBingoNum(newArr);
+    } catch (err) {
+      setErr(err.message);
+    }
+  };
 
   const fetchName = async (url) => {
     try {
@@ -58,24 +74,38 @@ export default function Main() {
     }
   };
 
-  if (loading) return <p>Loading...</p>;
   if (err) return <p>Error:{err}</p>;
-  console.log(pokeData);
-
-  function hundleClick() {
-    console.log(text);
-  }
+  //   console.log(pokeData);
 
   return (
     <div>
-      <input type="text" onChange={(e) => setText(e.target.value)} />
-      <button type="button" onClick={() => hundleClick()}>
+      <input
+        type="number"
+        min="1"
+        max="75"
+        onChange={(e) => e.target.value <= 75 && setNum(e.target.value)}
+      />
+      <button type="button" onClick={() => fetchData()}>
         click
       </button>
-      <p>{pokeData.species.name}</p>
-      <p>{pokeData.species.genus}</p>
-      <p>{pokeData.species.number}</p>
-      <p>{text}</p>
+      {num !== null && <p>図鑑番号：{num}</p>}
+      {pokeData.species.name !== null && (
+        <div>
+          <img src={pokeData.image} alt="image" />
+          <p>名前：{pokeData.species.name}</p>
+          <p>{pokeData.species.genus}</p>
+          <div className="flex gap-5 flex-wrap m-auto p-3">
+            {stockNum.map((data, num) => (
+              <p
+                key={num}
+                className=" items-center p-3 text-white font-bold bg-red-600 border-4 border-black text-2xl w-15 text-center rounded-full"
+              >
+                {data}
+              </p>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
