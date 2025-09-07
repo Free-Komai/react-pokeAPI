f5c742e8d44bc2857e8a07e79859f0e33c67f31f
freekomaiyasuaki <free.komaiyasuaki@gmail.com>
2025-09-07 21:40:28 +0900

    gradient add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className=" bg-pink-100 p-3 text-gray-800 font-bold mt-auto">
+    <div className="p-3 text-gray-800 font-bold mt-auto bg-gradient-to-t from-pink-200 to-white">
       <h1 className="text-center">@Free-Komai</h1>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="bg-pink-100 p-5 text-gray-800 font-bold text-3xl">
+    <div className="p-5 text-gray-800 font-bold text-3xl bg-gradient-to-b from-pink-200 to-white">
       <h1>ポケモンBINGOマシーン</h1>
     </div>
   );
